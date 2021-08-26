79e3528928644a62cca2b2fed43530af56e668af
Oliver Eisenberg <oseisenberg@gmail.com>
2021-08-26 10:34:04 -0700

    Runs simulation

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from simulation import Simulation
 
 if __name__ == '__main__':
     unit_data = UnitData()
-    unit_1 = Unit(1, "a", 1, (0, 0), [(1, 1), (2, 2), (3, 3)])
-    unit_2 = Unit(2, "b", 2, (0, 0), [(1, 1), (2, 2), (3, 3)])
-    unit_3 = Unit(3, "c", 3, (0, 0), [(1, 1), (2, 2), (3, 3)])
+    unit_1 = Unit(1, "a", 1, (0, 0), [(3, 3), (2, 2), (3, 3)])
+    unit_2 = Unit(2, "b", 2, (0, 0), [(3, 3), (2, 2), (3, 3)])
+    unit_3 = Unit(3, "c", 3, (0, 0), [(3, 3), (2, 2), (3, 3)])
     unit_data.add_unit(unit_1)
     unit_data.add_unit(unit_2)
     unit_data.add_unit(unit_3)
     unit_data.save_data()
 
     simulation = Simulation(unit_data)
-    simulation.run_simulation(0.01, 100)
+    simulation.run_simulation(0.01, .05)
     simulation.save_data()
--- simulation.py
+++ simulation.py
@@ -1,4 +1,5 @@
 import json
+import math
 from pathlib import Path
 
 
@@ -21,19 +22,45 @@ class Simulation:
     def calculate_paths(self):
         return self.unit_data.get_waypoints()
 
+    @staticmethod
+    def calculate_coord_change(current_coord, destination_coord):
+        triangle_dist_x = abs(current_coord[0] - destination_coord[0])
+        triangle_dist_y = abs(current_coord[1] - destination_coord[1])
+        ratio = triangle_dist_y / triangle_dist_x
+        sum_of_squares = 1 + ratio * ratio
+        common_denom = math.sqrt(1 / sum_of_squares)
+        change_x = common_denom
+        change_y = ratio * common_denom
+        return change_x, change_y
+
     def run_simulation(self, time_step, time_limit):
         calculated_paths = self.calculate_paths()
         current_positions = self.unit_data.get_starting_positions()
-        print(current_positions)
-        print(calculated_paths)
-        self.simulation_data["positions"] = [
+        current_time = 0
+        while current_time <= time_limit:
+            print(current_positions)
+            for key, current_coord in current_positions.items():
+                destination_coord = calculated_paths[key][0]
+                coord_change = self.calculate_coord_change(current_coord, destination_coord)
+
+                if current_coord[0] < destination_coord[0]:
+                    new_x = current_coord[0] + coord_change[0]
+                else:
+                    new_x = current_coord[0] - coord_change[0]
+                if current_coord[1] < destination_coord[1]:
+                    new_y = current_coord[1] + coord_change[1]
+                else:
+                    new_y = current_coord[1] - coord_change[1]
+                current_positions[key] = (new_x, new_y)
+            current_time += time_step
+
+        self.simulation_data["positions"] = []
+        self.simulation_data["positions"].append([
             {
                 "test": 1
             },
             {
                 "test": 2
             }
-        ]
-        self.simulation_data["positions"].append([
         ])
-        print(time_step)
+        #print(time_step)
