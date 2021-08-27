b1f2ab32b500007a7f499521f5899fdc27591096
Oliver Eisenberg <oseisenberg@gmail.com>
2021-08-26 19:02:01 -0700

    Standard radio edges added

--- graph_analyzer.py
+++ graph_analyzer.py
@@ -1,9 +1,13 @@
 import json
 import networkx as nx
+import math
 from networkx.readwrite import json_graph
 from util.file_operations import create_blank_file
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
+
+def calculate_dist_between_points(point_1, point_2):
+    return math.sqrt( ((point_1[0]-point_2[0])**2)+((point_1[1]-point_2[1])**2))
 
 def create_clique_from_nodes(graph, nodes):
     for current_index in range(len(nodes)):
@@ -26,12 +30,24 @@ def create_cellular_radio_edges(graph, cellular_zones):
                     break
     create_clique_from_nodes(graph, nodes)
 
-def run_graph_analysis(positions_history, units_controller, cellular_zones):
+def create_standard_radio_edges(graph, standard_radio_radius):
+    nodes_with_radios = [x for x, y in graph.nodes(data=True) if y['has_standard_radio'] == True]
+    for current_index in range(len(nodes_with_radios)):
+        for next_index in range(current_index + 1, len(nodes_with_radios)):
+            current_node_key = nodes_with_radios[current_index]
+            next_node_key = nodes_with_radios[next_index]
+            current_node = graph.nodes[nodes_with_radios[current_index]]
+            next_node = graph.nodes[nodes_with_radios[next_index]]
+            if calculate_dist_between_points(current_node["coord"], next_node["coord"]) < standard_radio_radius:
+                graph.add_edge(current_node_key, next_node_key)
+
+
+def run_graph_analysis(positions_history, units_controller, cellular_zones, standard_radio_radius):
     create_blank_file("generated_data", "networks.json")
     graph_template = nx.Graph()
     units_data = units_controller.get_units_data()
     for key, value in units_data.items():
-        graph_template.add_node(key, has_standard_radio=value.has_cellular_radio,
+        graph_template.add_node(key, has_standard_radio=value.has_standard_radio,
                                 has_cellular_radio=value.has_cellular_radio,
                                 has_satellite_link=value.has_satellite_link)
     create_satellite_edges(graph_template)
@@ -44,6 +60,7 @@ def run_graph_analysis(positions_history, units_controller, cellular_zones):
             graph.nodes[key]["coord"] = value
 
         create_cellular_radio_edges(graph, cellular_zones)
+        create_standard_radio_edges(graph, standard_radio_radius)
 
         # Print serializable graph data
         graph_data = json_graph.node_link_data(graph)
--- main.py
+++ main.py
@@ -7,22 +7,22 @@ from shapely.geometry.polygon import Polygon
 
 if __name__ == '__main__':
     units_controller = UnitsController()
-    unit_1 = Unit(1, "a", "a", False, True, False, (0, 0), [(1, 1), (3, 3), (5, 5)])
-    unit_2 = Unit(2, "b", "b", False, True, False, (0, 0), [(1, 1), (3, 3), (5, 5)])
-    unit_3 = Unit(3, "c", "c", False, True, False, (0, 0), [(10, 10), (20, 20), (30, 30)])
-    unit_4 = Unit(4, "d", "d", False, True, False, (0, 0), [(10, 10), (20, 20), (30, 30)])
+    unit_1 = Unit(1, "a", "type_a", True, False, False, (0, 0), [(1, 1), (3, 3), (5, 5)])
+    unit_2 = Unit(2, "b", "type_b", True, False, False, (0, 0), [(1, 1), (3, 3), (5, 5)])
+    unit_3 = Unit(3, "c", "type_c", True, False, False, (0, 0), [(10, 10), (20, 20), (30, 30)])
+    unit_4 = Unit(5, "d", "type_a", True, False, False, (0, 0), [(10, 10), (20, 20), (30, 30)])
     units_controller.add_unit(unit_1)
     units_controller.add_unit(unit_2)
     units_controller.add_unit(unit_3)
-    # units_controller.add_unit(unit_4)
+    units_controller.add_unit(unit_4)
     units_controller.save_data()
 
     simulation = Simulation(units_controller)
     positions_history = simulation.run_simulation(1, 10)
     simulation.save_data()
 
-    # cellular_zones = [Polygon[(0, 0), (0, 10), (10, 10), (10, 0)]]
     cellular_zones = []
     polygon = Polygon([(0, 0), (0, 10), (10, 10), (10, 0)])
     cellular_zones.append(polygon)
-    graph_analyzer.run_graph_analysis(positions_history, units_controller, cellular_zones)
+    standard_radio_radius = 3
+    graph_analyzer.run_graph_analysis(positions_history, units_controller, cellular_zones, standard_radio_radius)
