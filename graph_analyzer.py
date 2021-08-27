cbc02ea85189cac22d82166beaf8913499d2bb5f
Oliver Eisenberg <oseisenberg@gmail.com>
2021-08-26 17:50:21 -0700

    Adding cellular radio edges

--- graph_analyzer.py
+++ graph_analyzer.py
@@ -1,31 +1,51 @@
 import json
-from pathlib import Path
 import networkx as nx
 from networkx.readwrite import json_graph
 from util.file_operations import create_blank_file
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 
-def run_graph_analysis(positions_history, units_controller):
+def create_clique_from_nodes(graph, nodes):
+    for current_index in range(len(nodes)):
+        for next_index in range(current_index + 1, len(nodes)):
+            graph.add_edge(nodes[current_index], nodes[next_index])
 
+def create_satellite_edges(graph):
+    nodes_with_satellite = [x for x, y in graph.nodes(data=True) if y['has_satellite_link'] == True]
+    create_clique_from_nodes(graph, nodes_with_satellite)
+
+def create_cellular_radio_edges(graph, cellular_zones):
+    nodes = []
+    coords = nx.get_node_attributes(graph, 'coord')
+    has_cellular_radio_list = nx.get_node_attributes(graph, 'has_cellular_radio')
+    for node in graph.nodes:
+        if has_cellular_radio_list[node]:
+            for zone in cellular_zones:
+                if zone.contains(Point(coords[node])):
+                    nodes.append(node)
+                    break
+    create_clique_from_nodes(graph, nodes)
+
+def run_graph_analysis(positions_history, units_controller, cellular_zones):
     create_blank_file("generated_data", "networks.json")
     graph_template = nx.Graph()
     units_data = units_controller.get_units_data()
     for key, value in units_data.items():
         graph_template.add_node(key, has_standard_radio=value.has_cellular_radio,
-                                has_cellular_radio=value.has_cellular_radio, has_satellite_link=value.has_satellite_link)
+                                has_cellular_radio=value.has_cellular_radio,
+                                has_satellite_link=value.has_satellite_link)
+    create_satellite_edges(graph_template)
 
     networks_data = []
     for time_frame in positions_history:
-        # handle sattelite connections
         graph = graph_template.copy()
 
-        # Graph = nx.Graph()
-        # Graph.add_node(1)
-        # Graph.add_node(2)
-        # Graph.add_node(3)
-        # Graph.add_node(4)
-        # Graph.add_node(5)
-        # Graph.add_edges_from([(1,2),(2,3),(4,5)])
+        for key, value in time_frame.items():
+            graph.nodes[key]["coord"] = value
 
+        create_cellular_radio_edges(graph, cellular_zones)
+
+        # Print serializable graph data
         graph_data = json_graph.node_link_data(graph)
         networks_data.append(json.dumps(graph_data))
 
--- main.py
+++ main.py
@@ -2,19 +2,27 @@ import graph_analyzer
 from units_controller import *
 from data_structures.unit import Unit
 from simulation import Simulation
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 
 if __name__ == '__main__':
     units_controller = UnitsController()
-    unit_1 = Unit(1, "a", "a", False, False, True, (0, 0), [(1, 1), (3, 3), (5, 5)])
-    unit_2 = Unit(2, "b", "b", False, False, True, (0, 0), [(1, 1), (3, 3), (5, 5)])
-    unit_3 = Unit(3, "c", "c", False, False, True, (0, 0), [(10, 10), (20, 20), (30, 30)])
+    unit_1 = Unit(1, "a", "a", False, True, False, (0, 0), [(1, 1), (3, 3), (5, 5)])
+    unit_2 = Unit(2, "b", "b", False, True, False, (0, 0), [(1, 1), (3, 3), (5, 5)])
+    unit_3 = Unit(3, "c", "c", False, True, False, (0, 0), [(10, 10), (20, 20), (30, 30)])
+    unit_4 = Unit(4, "d", "d", False, True, False, (0, 0), [(10, 10), (20, 20), (30, 30)])
     units_controller.add_unit(unit_1)
     units_controller.add_unit(unit_2)
     units_controller.add_unit(unit_3)
+    # units_controller.add_unit(unit_4)
     units_controller.save_data()
 
     simulation = Simulation(units_controller)
     positions_history = simulation.run_simulation(1, 10)
     simulation.save_data()
 
-    graph_analyzer.run_graph_analysis(positions_history, units_controller)
+    # cellular_zones = [Polygon[(0, 0), (0, 10), (10, 10), (10, 0)]]
+    cellular_zones = []
+    polygon = Polygon([(0, 0), (0, 10), (10, 10), (10, 0)])
+    cellular_zones.append(polygon)
+    graph_analyzer.run_graph_analysis(positions_history, units_controller, cellular_zones)
