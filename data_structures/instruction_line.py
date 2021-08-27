9fd01817622e081af1f1af483ac33fdf80e4b072
Oliver Eisenberg <oseisenberg@gmail.com>
2021-08-27 05:52:26 -0700

    Waypoint instruction finished

--- data_structures/instruction_line.py
+++ data_structures/instruction_line.py
@@ -1,27 +1,5 @@
 import re
-
-
-def split_list_with_braces(string_to_split):
-    trailing_index = 0
-    param_list = []
-    paren_count = 0
-    bracket_count = 0
-    for index, char in enumerate(string_to_split):
-        if char == '(':
-            paren_count += 1
-        elif char == ')':
-            paren_count -= 1
-        elif char == '[':
-            bracket_count += 1
-        elif char == ']':
-            bracket_count -= 1
-        elif char == ',':
-            if paren_count == 0 and bracket_count == 0:
-                param_list.append(string_to_split[trailing_index:index])
-                trailing_index = index + 1
-    param_list.append(string_to_split[trailing_index:len(string_to_split)])
-    return param_list
-
+from util.util import split_list_with_braces
 
 class InstructionLine:
     def __init__(self, line):
@@ -38,22 +16,4 @@ class InstructionLine:
 
         parameters = split[-1][first_open_paren + 1:last_end_paren]
         param_list = split_list_with_braces(parameters)
-        # trailing_index = 0
-        # param_list = []
-        # paren_count = 0
-        # bracket_count = 0
-        # for index, char in enumerate(parameters):
-        #     if char == '(':
-        #         paren_count += 1
-        #     elif char == ')':
-        #         paren_count -= 1
-        #     elif char == '[':
-        #         bracket_count += 1
-        #     elif char == ']':
-        #         bracket_count -= 1
-        #     elif char == ',':
-        #         if paren_count == 0 and bracket_count == 0:
-        #             param_list.append(parameters[trailing_index:index])
-        #             trailing_index = index + 1
-        # param_list.append(parameters[trailing_index:len(parameters)])
         self.param_list = param_list if parameters else []
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from simulation import Simulation
 from shapely.geometry.polygon import Polygon
 from data_structures.instruction_line import InstructionLine
 import re
-import util.util
+from util.util import set_starting_positions, set_equipment, split_list_with_braces, set_waypoints
 
 
 def convert_to_units_list(value, var_to_units, var_to_group):
@@ -24,11 +24,31 @@ def generate_position(value, var_to_position):
     if value in var_to_position:
         return var_to_position[value]
     split = list(filter(None, re.split(r'\(|\)|,', value)))
-    return split[0], split[1]
+    return float(split[0]), float(split[1])
+
+
+def generate_position_list(value, var_to_position):
+    if value in var_to_position:
+        return var_to_position[value]
+    value = value[1:len(value) - 1]
+    position_tuple_list = []
+    for position_string in split_list_with_braces(value):
+        position_tuple_list.append(generate_position(position_string, var_to_position))
+    return position_tuple_list
+
+
+def remove_quotes(value):
+    return value.replace("\"", "")
 
 
 if __name__ == '__main__':
     units_controller = UnitsController()
+    cellular_zones = []
+
+    # Default values, unless set directly in instructions
+    time_duration = 10
+    time_step = 1
+    standard_radio_radius = 3
 
     var_to_units = {}
     var_to_group = {}
@@ -43,27 +63,40 @@ if __name__ == '__main__':
             function_name = instruction_line.function_name
             param_list = instruction_line.param_list
 
+            # delete
             print(initialized_variable, function_name, param_list, flush=True)
+            # delete
 
             if function_name == "create_units":
-                unit_name = param_list[0]
-                unit_type = param_list[1]
+                unit_name = remove_quotes(param_list[0])
+                unit_type = remove_quotes(param_list[1])
                 unit_count = int(param_list[2])
                 units_created = units_controller.create_unit(unit_name, unit_type, unit_count)
                 var_to_units[initialized_variable] = units_created
+            elif function_name == "set_time":
+                attribute = remove_quotes(param_list[0])
+                if attribute == "duration":
+                    time_duration = float(param_list[1])
+                elif attribute == "step":
+                    time_step = float(param_list[1])
             elif function_name == "equip":
                 units_list = convert_to_units_list(param_list[0], var_to_units, var_to_group)
                 if param_list[1] == "standard_radio":
-                    util.util.set_equipment(units_list, True, False, False)
+                    set_equipment(units_list, True, False, False)
                 if param_list[1] == "cellular_radio":
-                    util.util.set_equipment(units_list, False, True, False)
+                    set_equipment(units_list, False, True, False)
                 if param_list[1] == "satellite_link":
-                    util.util.set_equipment(units_list, False, False, True)
-            # elif function_name == "create_cellular_region":
-            #     position_list = generate_position_list(param_list[0])
+                    set_equipment(units_list, False, False, True)
+            elif function_name == "create_cellular_region":
+                position_list = generate_position_list(param_list[0], var_to_position)
+                polygon = Polygon(position_list)
+                cellular_zones.append(polygon)
             elif function_name == "create_position":
                 position = generate_position(param_list[0], var_to_position)
                 var_to_position[initialized_variable] = position
+            elif function_name == "create_position_list":
+                position_list = generate_position_list(param_list[0])
+                var_to_position[initialized_variable] = position_list
             elif function_name == "create_group":
                 units_in_group = []
                 for param in param_list:
@@ -74,22 +107,14 @@ if __name__ == '__main__':
             elif function_name == "set_starting_position":
                 units_list = convert_to_units_list(param_list[0], var_to_units, var_to_group)
                 starting_position = generate_position(param_list[1], var_to_position)
-                util.util.set_starting_positions(units_list, starting_position)
+                set_starting_positions(units_list, starting_position)
+            elif function_name == "set_waypoints":
+                units_list = convert_to_units_list(param_list[0], var_to_units, var_to_group)
+                position_list = generate_position_list(param_list[1], var_to_position)
+                set_waypoints(units_list, position_list)
 
-    # units_controller.create_unit("pedestrian", "type_a", 1)
-
-    # units_controller.create_unit("name_a", "type_a", 1, (0, 0), [(1, 1), (3, 3), (5, 5)], True)
-    # units_controller.create_unit("name_b", "type_b", 1, (0, 0), [(1, 1), (3, 3), (5, 5)], True)
-    # units_controller.create_unit("name_c", "type_c", 1, (0, 0), [(10, 10), (20, 20), (30, 30)], True)
-    # units_controller.create_unit("name_d", "type_a", 1, (0, 0), [(10, 10), (20, 20), (30, 30)], True)
     units_controller.save_data()
-
     simulation = Simulation(units_controller)
-    positions_history = simulation.run_simulation(1, 10)
+    positions_history = simulation.run_simulation(time_step, time_duration)
     simulation.save_data()
-
-    cellular_zones = []
-    polygon = Polygon([(0, 0), (0, 10), (10, 10), (10, 0)])
-    cellular_zones.append(polygon)
-    standard_radio_radius = 3
     graph_analyzer.run_graph_analysis(positions_history, units_controller, cellular_zones, standard_radio_radius)
