ff4907543396054d11a5320a717a9f6f0fbdfd8c
Oliver Eisenberg <oseisenberg@gmail.com>
2021-11-29 16:25:40 +0000

    Improving graphics and ui

--- mobscript/display_controller.py
+++ mobscript/display_controller.py
@@ -2,6 +2,10 @@ from typing import Callable, Dict, List, Tuple, Union
 import pygame
 import math
 from pygame.locals import QUIT
+import pygame_widgets
+from pygame_widgets.slider import Slider
+from pygame_widgets.textbox import TextBox
+from pygame_widgets.button import Button
 from .data_structures.global_attributes import GlobalAttributes
 from .data_structures.unit import Unit
 from .map_controller import MapController
@@ -24,8 +28,9 @@ class UnitSprite():
 
     def update(self, curr_time: Union[int, float]) -> None:
         time_index = math.floor(curr_time // self.time_step)
-        self.curr_pos = self.positions_history[time_index][self.unit_key]
-        self.curr_pos = (self.curr_pos[1], self.curr_pos[0])
+        if time_index < len(self.positions_history):
+            self.curr_pos = self.positions_history[time_index][self.unit_key]
+            self.curr_pos = (self.curr_pos[1], self.curr_pos[0])
 
     def display(self, 
                 convert_coord_func: Callable[[Tuple[int, int], Tuple[int, int], int], Tuple[int, int]], 
@@ -33,10 +38,13 @@ class UnitSprite():
         screen_width, screen_height = self.screen.get_size()
         converted_coord = convert_coord_func(self.curr_pos, origin, screen_height)
         pygame.draw.circle(self.screen, (0, 0, 255), converted_coord, self.radius)
-        # pygame.draw.rect(self.screen, (0, 0, 255), [converted_coord[0], converted_coord[1], 50, 50])
 
 
 class DisplayController:
+    SCREEN_WIDTH = 800
+    SCREEN_HEIGHT = 800
+    char_to_image = {}
+
     def __init__(self, 
                  units_controller: UnitsController, 
                  map_controller: MapController, 
@@ -49,6 +57,10 @@ class DisplayController:
         self.scale = self.map_controller.get_scale()
         self.length_to_pixels = self.global_attributes.length_to_pixels
         self.multiplier = self.global_attributes.length_to_pixels * self.scale
+
+        self.char_to_image['^'] = pygame.image.load("mobile_network_scripting/images/mountain.png")
+        self.char_to_image['_'] = pygame.image.load("mobile_network_scripting/images/ground.png")
+        self.char_to_image['r'] = pygame.image.load("mobile_network_scripting/images/road.png")
 
     def convert_coord(self, 
                       coord: Tuple[int, int], 
@@ -69,13 +81,50 @@ class DisplayController:
                 pos_rect = self.convert_coord((self.scale * col, self.scale * row + self.scale), origin, screen_height)
                 pos_char = self.convert_coord((self.scale * col + self.scale / 2, self.scale * row + self.scale / 2),
                                               origin, screen_height)
-                pygame.draw.rect(screen, color, [pos_rect[0], pos_rect[1], self.multiplier, self.multiplier])
-                screen.blit(font.render(map[row][col], True, (255, 0, 0)), pos_char)
+                # pygame.draw.rect(screen, color, [pos_rect[0], pos_rect[1], self.multiplier, self.multiplier])
+                rect = [pos_rect[0], pos_rect[1], self.multiplier, self.multiplier]
+                curr_char = map[row][col]
+                screen.blit(pygame.transform.smoothscale(
+                    self.char_to_image[curr_char], (self.multiplier, self.multiplier)), rect)
+                # screen.blit(font.render(map[row][col], True, (255, 0, 0)), pos_char)
 
     def display(self) -> None:
         pygame.init()
-        screen = pygame.display.set_mode((800, 600))
+        screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
         origin = (0, 0)
+        current_time = 0.0
+        paused = False
+        clock = pygame.time.Clock()
+
+        slider = Slider(screen, 100, 30, self.SCREEN_WIDTH - 310, 20, step=.001, initial=0, min=0, 
+            max=self.global_attributes.time_duration)
+        button = Button(screen, 40, 27, 30, 30,
+            onClick=lambda: (paused := not paused))
+
+        time_duration_text = TextBox(screen, self.SCREEN_WIDTH - 180, 10, 180, 30,
+            fontSize=24, borderThickness=0, textColour=(50, 50, 50))
+        time_duration_text.setText("Time Duration: {}s".format((self.global_attributes.time_duration)))
+        time_duration_text.disable()
+        current_time_text = TextBox(screen, self.SCREEN_WIDTH - 180, 35, 180, 30,
+            fontSize=24, borderThickness=0, textColour=(50, 50, 50))
+        current_time_text.setText("Current Time: {}s".format(current_time))
+        current_time_text.disable()
+
+        instructions_text_1 = TextBox(screen, self.SCREEN_WIDTH - 180, 80, 180, 30,
+            fontSize=24, borderThickness=0, textColour=(50, 50, 50))
+        instructions_text_1.setText("i - Toggle Instructions")
+        instructions_text_1.disable()
+
+        instructions_text_2 = TextBox(screen, self.SCREEN_WIDTH - 180, 105, 180, 30,
+            fontSize=24, borderThickness=0, textColour=(50, 50, 50))
+        instructions_text_2.setText("arrows - Move screen")
+        instructions_text_2.disable()
+
+        instructions_text_3 = TextBox(screen, self.SCREEN_WIDTH - 180, 130, 180, 30,
+            fontSize=24, borderThickness=0, textColour=(50, 50, 50))
+        instructions_text_3.setText("space - toggle pause")
+        instructions_text_3.disable()
+
 
         unit_sprites = {}
         units_data = self.units_controller.get_units_data()
@@ -88,18 +137,33 @@ class DisplayController:
                 self.global_attributes.time_step
             )
 
-        start_time = pygame.time.get_ticks()
-
         display_on = True
+        display_instructions = True
+        slider_changed = False
         while display_on:
-            frame_time = pygame.time.get_ticks()
+            current_time_text.setText("Current Time: {:.0f}s".format(current_time))
+            clock.tick()
+            time_progression = clock.get_time() / 1000
+            slider_time = slider.getValue()
+
+            if not paused and abs(slider_time - current_time) < .0001 and current_time < self.global_attributes.time_duration:
+                current_time = current_time + time_progression
+                slider.setValue(current_time)
+                slider_changed = False
+            else:
+                if current_time != slider_time:
+                    slider_changed = True
+                current_time = slider_time            
+
             screen.fill((255, 255, 255))
             self.draw_map(screen, origin)
             for unit_sprite in unit_sprites.values():
-                unit_sprite.update((frame_time - start_time) / 1000.0)
+                if not paused or slider_changed:
+                    unit_sprite.update(current_time)
                 unit_sprite.display(self.convert_coord, origin)
 
-            for event in pygame.event.get():
+            events = pygame.event.get()
+            for event in events:
                 if event.type == QUIT:
                     display_on = False
                 elif event.type == pygame.KEYDOWN:
@@ -111,4 +175,18 @@ class DisplayController:
                         origin = origin[0], origin[1] - self.multiplier
                     if event.key == pygame.K_DOWN:
                         origin = origin[0], origin[1] + self.multiplier
+                    if event.key == pygame.K_SPACE:
+                        paused = not paused
+                    if event.key == pygame.K_i:
+                        if display_instructions == True:
+                            display_instructions = False
+                            instructions_text_1.hide()
+                            instructions_text_2.hide()
+                            instructions_text_3.hide()
+                        else:
+                            display_instructions = True
+                            instructions_text_1.show()
+                            instructions_text_2.show()
+                            instructions_text_3.show()
+            pygame_widgets.update(events)
             pygame.display.flip()
